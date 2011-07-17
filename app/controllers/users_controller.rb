75126802789a728ae17ad0328e9736bf47fe26bb
ankuramzn <ankur.saksena@gmail.com>
2011-07-17 16:12:13 -0700

    Minor fixes to user controller and fixing the side bar menu items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:type] = "user"
-      session[:id] = user.id
-      redirect_to vendors_list_path, :notice => "Thanks for Registering!!!"
+      session[:id] = @user.id
+      redirect_to user_home_path, :notice => "Thanks for Registering!!!"
     else
       render "new"
     end
