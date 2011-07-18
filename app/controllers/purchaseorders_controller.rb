7760a59def22d6c7fe90692635dece01e6fd8393
ankuramzn <ankur.saksena@gmail.com>
2011-07-18 15:12:39 -0700

    Changes to support 1. Adhoc creation of a Compliance Set by Vendor 2. Research Purchase orders by PO Id 3. Enable rendering Compliance Set creation form without prepopulated ASIN and grab the Vendor from session 4. Menu entries added for Vendor and User 5. Search form for Purchase orders 6. Routes

--- app/controllers/purchaseorders_controller.rb
+++ app/controllers/purchaseorders_controller.rb
@@ -1,10 +1,20 @@
 class PurchaseordersController < ApplicationController
+
+
+  #  TODO Only certain parties should be able to use thins function
   def index
     @purchaseorders = Purchaseorder.all
   end
 
   def show
-    @purchaseorder = Purchaseorder.find(params[:id])
+    if params[:id]
+      @purchaseorder = Purchaseorder.find(params[:id]) unless (session[:type] == "vendor" && Purchaseorder.find(params[:id]).vendor_id != session[:id])
+    elsif params[:purchaseorder_id]
+      @purchaseorder = Purchaseorder.find_by_code(params[:purchaseorder_id]) unless (session[:type] == "vendor" && Purchaseorder.find_by_code(params[:purchaseorder_id]).vendor_id != session[:id])
+    end
+    if @purchaseorder.nil?
+      redirect_to research_purchaseorder_path, :notice => "The requested Purchase Order could not be found"
+    end
   end
 
   def purchaseorder_asins
@@ -14,4 +24,8 @@ class PurchaseordersController < ApplicationController
         format.js {  }
     end
   end
+
+  def research
+  end
+
 end
