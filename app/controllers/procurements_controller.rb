e93db89320fc72786f1b2c0cf86eb323d570a4e3
ankuramzn <ankur.saksena@gmail.com>
2011-08-23 11:36:03 -0700

    Improving the layout of the bulk_process by adding processing status color, and added a class method to process in bulk

--- app/controllers/procurements_controller.rb
+++ app/controllers/procurements_controller.rb
@@ -25,9 +25,19 @@ class ProcurementsController < ApplicationController
         :skip_blanks => true) do |row|
       row_hash = row.to_hash
       procurement =  Procurement.new(row_hash["vendor"], row_hash["purchaseorder"], row_hash["asin"], row_hash["date"])
-      procurement.process # Adds values to the database
+      #procurement.process # Adds values to the database
       @procurements << procurement
     end
+
+    @procurements = Procurement.procurement_process(@procurements)
+
+    # Upload file to s3
+    bucket_name = "procurement-data/" + (Time.now.to_i).to_s
+    aws_connection_instance = AmazonS3Asset.new
+    aws_connection_instance.store_object(bucket_name, params[:attachment].original_filename, params[:attachment].open)
+    @s3_url = "http://s3.amazonaws.com/" + bucket_name + "/" + params[:attachment].original_filename
+    puts @s3_url
+
   end
 
 end
--- app/models/procurement.rb
+++ app/models/procurement.rb
@@ -1,6 +1,6 @@
 class Procurement
 
-  attr_accessor :vendor_code, :purchaseorder_id, :sku, :date
+  attr_accessor :vendor_code, :purchaseorder_id, :sku, :date, :processed_status
 
   def initialize(vendor_code, purchaseorder_id, sku, date)
     @vendor_code, @purchaseorder_id, @sku, @date = vendor_code, purchaseorder_id, sku, date
@@ -13,27 +13,57 @@ class Procurement
   #  purchaseorder
   #  asin
   #  data
-  def process
-    vendor = Vendor.find_by_code(self.vendor_code)
-    return unless vendor
-    purchaseorder = Purchaseorder.search(self.purchaseorder_id, vendor.id)
-    purchaseorder = vendor.purchaseorders.create!(
-        :code => self.purchaseorder_id,
-        :condition=>'incomplete',
-        :delivery_date=>Date.parse(self.date).to_time,
-        :marketplace=>'EU',
-        :status=>'pending_vendor_input'
-    ) unless purchaseorder
+  # Return Cases:
+  #  Vendor Code not found in the system -> Red
+  #  Vendor Code found, purchaseorder code not found in the system => Asins dont care -> Green
+  #  Vendor Code found, purchaseorder code found in the system asin not found in the system -> Yellow
+  #  Vendor Code found, purchaseorder code found in the system asin found in the system => existing record -> Blue
 
-    asin = Asin.search(self.sku, purchaseorder.id)
+  def self.procurement_process(procurements)
 
-    asin = purchaseorder.asins.create!(
-        :sku => self.sku,
-        :title => 'Missing Title',
-        :srq_impact_source => 'default',
-        :status=>'pending_vendor_input'
-    ) unless asin
+    new_purchaseorders = Array.new
+    procurements.each do |procurement|
 
+      puts "Processing " + procurement.inspect
+      vendor = Vendor.find_by_code(procurement.vendor_code)
+      if vendor.nil?
+        # Unknown vendor => no need to process this procurement
+        puts "Unknown Vendor " + procurement.vendor_code
+        procurement.processed_status = '#FF005D' # Red
+        next
+      else
+        purchaseorder = Purchaseorder.search(procurement.purchaseorder_id, vendor.id)
+        if purchaseorder.nil? || new_purchaseorders.include?(procurement.purchaseorder_id)
+          puts "New Purchase Order " + procurement.purchaseorder_id
+          # Unknown purchase order => Create both purchase order and asin
+          procurement.processed_status = '#B4F263' # Green
+          purchaseorder = vendor.purchaseorders.create!(
+              :code => procurement.purchaseorder_id,
+              :condition=>'incomplete',
+              :delivery_date=>Date.parse(procurement.date).to_time,
+              :marketplace=>'EU',
+              :status=>'pending_vendor_input'
+          ) unless purchaseorder
+          new_purchaseorders << procurement.purchaseorder_id
+        end
+
+
+        asin = Asin.search(procurement.sku, purchaseorder.id)
+        if asin.nil?
+          puts "New ASIN " + procurement.sku
+          procurement.processed_status = '#EDED8C' unless procurement.processed_status.eql?('#B4F263') # Yellow
+          asin = purchaseorder.asins.create!(
+              :sku => procurement.sku,
+              :title => 'Missing Title',
+              :srq_impact_source => 'default',
+              :status=>'pending_vendor_input'
+          )
+        next
+        end
+        procurement.processed_status = '#B8EAF5' # Blue
+      end
+    end
+
+    return procurements
   end
-
 end
